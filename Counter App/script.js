9b6ad1a5ec5b8af7877294348a19dac389d81f03
Sushil <kidsushilink@gmail.com>
2024-02-14 20:11:51 -0800

    Counter App
    -- which lets you increase, reset, decrease

---
+++ Counter App/script.js
@@ -0,0 +1,25 @@
+const decrement = document.querySelector("#btn1");
+const reset = document.querySelector("#btn2");
+const increment = document.querySelector("#btn3");
+const innerTxt = document.querySelector("#mark");
+let val = 0;
+
+decrement.addEventListener("click", 
+    decrease = () =>  {
+        val -= 1;
+        innerTxt.innerText = val;
+});
+
+reset.addEventListener("click", 
+    re = () => {
+        val = 0;
+        innerTxt.innerText = val;
+});
+
+increment.addEventListener("click", 
+    increase = () => {
+        val += 1;
+        innerTxt.innerText = val;
+    
+});
+
