8515c1a1f451328d57bbd7cb583e371af747c8ce
Sushil <kidsushilink@gmail.com>
2024-02-16 22:32:34 -0800

    remaining
    -- pending

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,11 +1,30 @@
-const btn1 = document.querySelector(".one");
-const inpu = document.querySelector(".inp");
+const btn1 = document.querySelector(".re");
+const btn2 = document.querySelector(".add");
+const btn3 = document.querySelector(".divide");
+const btn4 = document.querySelector(".seven");
 
-console.log(inpu);
-console.log(btn1);
-let val = "";
+
 btn1.addEventListener("click", reset = () => {
     val = "";
-    inpu.innerText = val;
+    // inpu.innerText = val;
     console.log("reset clicked");
 })
+
+
+btn2.addEventListener("click", add = () => {
+    console.log("add clicked");
+})
+
+
+btn3.addEventListener("click", vDivide = () => {
+    console.log("divide clicked");
+})
+
+
+// btn4.addEventListener("click", vSeven = () => {
+//     console.log("Seven Clicked");
+// })
+
+
+
+
