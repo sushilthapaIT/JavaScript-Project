3057e22e85c91630a7c49094d1623db64be4815c
Sushil <kidsushilink@gmail.com>
2024-02-27 00:25:46 -0800

    update

--- TEST FILES/Input Test/script.js
+++ TEST FILES/Input Test/script.js
@@ -1,11 +1,103 @@
-function testing(){
-    var input1 = document.getElementById("testing");
-    var input2 = document.getElementById("testing1");
-    var output = input1.value * input2.value;
+// function testing(){
+//     var input1 = document.getElementById("testing");
+//     var input2 = document.getElementById("testing1");
+//     var output = input1.value * input2.value;
 
-    var output1 = document.getElementById("output1")
-    output1.innerText = `<p style="color:red;">${output}</p>`
+//     var output1 = document.getElementById("output1")
+//     output1.innerText = `<p style="color:red;">${output}</p>`
 
 
-    console.log("Hello",input1)
+//     console.log("Hello",input1)
+// }
+
+
+
+// Program to display the weather information for the given city
+
+// vairiable declerations
+const cityInputContainer = document.getElementById("cityInput");
+const weatherInfoContainer = document.getElementById("weatherInfo");
+
+// variable for api request
+const apiKey = "Your API KEY";
+const apiEndpoint = 'https://api.weatherapi.com/v1/current.json';
+
+// function for constructing full api
+function constructUrl(city){
+    return `${apiEndpoint}?key=${apiKey}&q=${city}`;
 }
+
+function clearWeatherInfo() {
+    weatherInfoContainer.innerHTML = '';
+}
+
+// function for handling the error and displaying the error message
+function handleError(message){
+    weatherInfoContainer.innerHTML = `<p style="color: red;">${message}</p>`;
+}
+
+// closures in js
+function logCity(city){
+    return function(){
+        console.log(`The city is: ${city}`);
+    };
+};
+
+
+// Getting input from the user and validating it
+function getWeather(){
+    const city = cityInputContainer.value.trim();
+    if(!city){
+        alert("Please enter a city");
+        return;
+    }
+
+    clearWeatherInfo();
+    // loging the trimmed value of the city, not necessary but for the purpose of assignment
+    const logCityCallback = logCity(city); // logCity(city) returns the function, so logCityCallback is the function
+    fetchAndDisplayWeather(city, logCityCallback);
+
+}
+
+// use of promises
+function fetchWeatherData(city){
+    return new Promise(async (resolve, reject) => {
+        try {
+            const apiUrl = constructUrl(city);
+            const response = await fetch(apiUrl);
+            const responseData = await response.json();
+            resolve(responseData);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+// calling callback after fetching weather data
+function fetchAndDisplayWeather(city, callback){
+    fetchWeatherData(city)
+        .then((data)=>{
+            displayWeatherInfo(data);
+            callback();
+        })
+        .catch((error)=>{
+            handleError(error);
+            callback();
+        })
+}
+
+//Displaying the weather
+function displayWeatherInfo(weatherData) {
+    const  {location, current}  = weatherData;
+    try{
+      const info = `
+      <h2>Weather in ${location.name}, ${location.country}</h2>
+      <p>Temperature: ${current.temp_c}°C</p>
+      <p>Condition: ${current.condition.text}</p>
+      <p>Wind Speed: ${current.wind_kph} kph</p>
+    `;
+    weatherInfo.innerHTML = info;
+    }catch(error){
+        handleError("The city does not exist");
+    };
+  };
--- Weather App/script.js
+++ Weather App/script.js
@@ -1,7 +1,7 @@
+// javascript
 
-
-
-
+const apiKey = "4eaadd3e6d8b7fadae1d69e277294842";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
@@ -21,25 +21,25 @@ async function checkWeather(city) {
 
         else
         {
-
             document.querySelector(".city").innerHTML = data.name;
             document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + "°C";
             document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
             document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
             document.querySelector(".feels-like").innerHTML = Math.round(data.main.feels_like) + "°C";
             document.querySelector(".pressure").innerHTML = data.main.pressure + " hPa";
+            document.querySelector(".country-code").innerHTML = data.sys.country;
 
             if(data.weather[0].main == "Clouds")
             {
                 weatherIcon.src = "images/clouds.png";
                 weatherInfo.innerHTML = "Cloudy";
             }
-            else if(data.weather[0].main =="Clear")
+            else if(data.weather[0].main == "Clear")
             {
                 weatherIcon.src = "images/clear.png";
                 weatherInfo.innerHTML = "Clear";
             }
-            else if(data.weather[0].main =="Rain")
+            else if(data.weather[0].main == "Rain")
             {
                 weatherIcon.src = "images/rain.png";
                 weatherInfo.innerHTML = "Rainy";
@@ -58,11 +58,8 @@ async function checkWeather(city) {
             document.querySelector(".weather").style.display = "block";
             document.querySelector(".error").style.display = "none";
         }
-    
     }
-
 
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
 })
-
