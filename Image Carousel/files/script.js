61172435f644d0f6b4b36e6fd6f26048a7e12b73
Sushil <kidsushilink@gmail.com>
2024-03-07 23:39:22 -0800

    update

--- Image Carousel/files/script.js
+++ Image Carousel/files/script.js
@@ -1,1 +1,17 @@
-c
+let index = 0;
+
+const prev = document.querySelector("#previous");
+const next = document.querySelector("#next");
+
+prev.addEventListener("click", () => {
+    index -= 1;
+
+    let images = document.querySelector("img");
+    let dots = document.querySelector("dot");
+    console.log("previous clicked.");
+})
+
+next.addEventListener("click", () => {
+    index += 1;
+    console.log("next clicked");
+})
