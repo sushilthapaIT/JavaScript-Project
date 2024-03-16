ad3fafb5dd541c6bbba0d71367cac618d6898c51
Sushil <kidsushilink@gmail.com>
2024-03-16 01:02:07 -0700

    update

--- JS Refresh/Chapter 12/async.js
+++ JS Refresh/Chapter 12/async.js
@@ -10,6 +10,7 @@ we can make any normal function to async function
 */
 
 
+// basic example of api call
 function api(){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -19,7 +20,35 @@ function api(){
     })
 }
 
+async function getWeather(){
+    await api(); //1st call
+    await api(); //2nd call
+}
+
 async function hello(){
     console.log("Hello World")
 }
 
+
+ /* async-await */
+function getData(dataId){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data: ", dataId);
+            resolve("success");
+        }, 2000);
+    })
+}
+
+async function getAllData() {
+    await getData(1);
+    await getData(2);
+    await getData(3);
+}
+
+// //iife - immediately invoked function expression
+(async function getAllData() {
+    await getData(1);
+    await getData(2);
+    await getData(3);
+})()
