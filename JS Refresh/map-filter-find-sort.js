c85e2da4412b022afa52c2d435477ca527734ff8
Sushil <kidsushilink@gmail.com>
2024-03-16 23:44:17 -0700

    started leaning about map, filter function

--- JS Refresh/map-filter-find-sort.js
+++ JS Refresh/map-filter-find-sort.js
@@ -1,1 +1,13 @@
 // map, filter, find, sort
+
+/*
+map() - creates a new array by performing some operation on each array element
+it is mainly used in transformation suppose to make a 
+arr = [5, 1, 3, 2, 6]
+double = [10, 2, 6, 4, 12]
+triple = [15, 3, 9, 6, 18]
+binary = ["101", "1", "11", "10", "110"]
+*/
+
+
+
