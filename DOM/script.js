e7c2c12e7459b51cadce8c4264b2c4ced8890e90
Sushil <kidsushilink@gmail.com>
2024-03-18 01:12:22 -0700

    dom

---
+++ DOM/script.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector("#button");
+const firNum = document.querySelector("#fnum");
+const sirNum = document.querySelector("#snum");
+const res = document.querySelector("#output");
+
+
+function add(a, b){
+    return a + b;
+}
+
+btn.addEventListener("click", () => {
+    let one = parseInt(firNum.value);
+    let two = parseInt(sirNum.value);
+    let sum = add(one, two)
+    res.innerText = `Sum is ${sum}`;
+});
